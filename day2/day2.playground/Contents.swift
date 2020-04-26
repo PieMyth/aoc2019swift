af3bed83e6c0450b1457f1cd4d82ed27e1219b56
William Haugen <haugen@pdx.edu>
2020-04-25 22:29:39 -0700

    day2 part2

--- day2/day2.playground/Contents.swift
+++ day2/day2.playground/Contents.swift
@@ -3,17 +3,11 @@ import Cocoa
 let inputOfFile = try! String(contentsOfFile: "/Users/williamhaugen/dev/aoc2019swift/day2/input.txt")
 let lines = inputOfFile.split(separator: ",")
 
-var numbers = lines.map({Int($0)!})
-
-//var numbers = [1,9,10,3,2,3,11,0,99,30,40,50]
-var counter = 0
 var index = 0
-numbers[1] = 12
-numbers[2] = 2
+var numbers = lines.map({Int($0)!})
 while index < numbers.count-1 {
     let opcode = numbers[index]
     if opcode == 99 {
-        print("99 at index \(index)")
         break
     }
     index += 1
@@ -23,21 +17,57 @@ while index < numbers.count-1 {
     index += 1
     let thirdindex = numbers[index]
     index += 1
-    print("\(opcode) \(firstindex) \(secondindex) \(thirdindex)")
     switch opcode{
     case 1:
-        print ("\(numbers[firstindex]) [\(firstindex)] + \(numbers[secondindex]) [\(secondindex)] on index \(thirdindex)")
         numbers[thirdindex] = numbers[firstindex] + numbers[secondindex]
         break
     case 2:
-        print ("\(numbers[firstindex]) [\(firstindex)] * \(numbers[secondindex]) [\(secondindex)] on index \(thirdindex)")
         numbers[thirdindex] = numbers[firstindex] * numbers[secondindex]
         break
     case _:
         print(index)
         print("Unknown opcode: \(opcode)")
-        print("\(numbers[0])")
     }
 }
-print(numbers.count-1)
-print("Number: \(numbers[0])")
+print("First question: \(numbers[0])")
+
+for firstadjust in 0...99{
+    for secondadjust in 0...99{
+        var numbers = lines.map({Int($0)!})
+        numbers[1] = firstadjust
+        numbers[2] = secondadjust
+        var index = 0
+        while index < numbers.count-1 {
+            let opcode = numbers[index]
+            if opcode == 99 {
+                break
+            }
+            index += 1
+            let firstindex = numbers[index]
+            index += 1
+            let secondindex = numbers[index]
+            index += 1
+            let thirdindex = numbers[index]
+            index += 1
+            switch opcode{
+            case 1:
+                numbers[thirdindex] = numbers[firstindex] + numbers[secondindex]
+                break
+            case 2:
+                numbers[thirdindex] = numbers[firstindex] * numbers[secondindex]
+                break
+            case _:
+                print(index)
+                print("Unknown opcode: \(opcode)")
+            }
+        }
+        if numbers[0] == 19690720{
+            print("Second question:\nFirst Number: \(firstadjust) Second Number: \(secondadjust)")
+            print("Number: \(numbers[0])")
+            break
+        }
+    }
+    if numbers[0] == 19690720{
+        break
+    }
+}
