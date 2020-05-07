17ae4bcf2db946d039040de6c3ce82d91719e8cb
William Haugen <haugen@pdx.edu>
2020-05-06 20:24:06 -0700

    solved day 4 part 2

--- day4/day4.playground/Contents.swift
+++ day4/day4.playground/Contents.swift
@@ -1,6 +1,6 @@
 import Cocoa
 
-func validPassword(numStr: String)->Bool{
+func validPassword(numStr: String, twoPairOnly: Bool)->Bool{
     var ascendingOnly = true
     var pair = false
     
@@ -10,13 +10,31 @@ func validPassword(numStr: String)->Bool{
     }
     
     if (ascendingOnly == true){
-        for i in 1..<numStr.count{
-            let firstIndex = numStr.index(numStr.startIndex, offsetBy: i-1)
-            let secondIndex = numStr.index(numStr.startIndex, offsetBy: i)
+        var pairNumber = [Int:Int]()
+        for i in 0..<numStr.count{
+            let firstIndex = numStr.index(numStr.startIndex, offsetBy: i)
             let first: Int = numStr[firstIndex].wholeNumberValue!
-            let second: Int = numStr[secondIndex].wholeNumberValue!
-            if (!pair && first == second){
-                pair = true
+            if (pairNumber[first] == nil){
+                pairNumber[first] = 1
+            }
+            else {
+                pairNumber[first]? += 1
+            }
+        }
+        if (twoPairOnly == true){
+            for (_,count) in pairNumber{
+                if (count == 2){
+                    pair = true
+                    break
+                }
+            }
+        }
+        else {
+            for (_,count) in pairNumber{
+                if (count >= 2){
+                    pair = true
+                    break
+                }
             }
         }
     }
@@ -29,8 +47,17 @@ let testPass = ["111111", "223450", "123789"]
 let testResults = [ true, false, false]
 for i in 0..<testPass.count{
     print("\(testPass[i]) is supposed to be \(testResults[i])", terminator: " ")
-    print(": \(validPassword(numStr: testPass[i]))")
+    print(": \(validPassword(numStr:testPass[i], twoPairOnly: false))")
 }
+
+print("TWO PAIR ONLY")
+let testTwoPairOnly = ["112233", "123444", "111122"]
+let testTwoPairResult = [true, false, true]
+for i in 0..<testTwoPairOnly.count{
+    print("\(testTwoPairOnly[i]) is supposed to be \(testTwoPairResult[i])", terminator: " ")
+    print(": \(validPassword(numStr: testTwoPairOnly[i], twoPairOnly: true)) ")
+}
+
 print()
 
 let minNumber = 123257
@@ -41,7 +68,7 @@ print("NUMBER TO TEST")
 print("\(maxNumber - minNumber)")
 for i in minNumber..<maxNumber{
     let numberString = String(i)
-    if (validPassword(numStr: numberString)){
+    if (validPassword(numStr: numberString, twoPairOnly:true)){
         count += 1
     }
 }
